4bcddc30c585f52f48f0dffa4ecaacf53f97f0b0
4sela <adawa21@student.sdu.dk>
2024-11-07 09:11:56 +0100

    Level Selector + more

---
+++ js/load-content.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function loadHTML(elementID, filePath) {
+        const element = document.getElementById(elementID);
+        if (element) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', filePath, true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    element.innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
+        }
+    }
+    loadHTML('header-container', '../header.html');
+});
---
+++ js/navigate.js
@@ -0,0 +1,8 @@
+function navigateToLevel(mapID) {
+    const path = 'level-' + mapID
+    window.location.href = '../levels/' + path + '.html'
+}
+
+function navigateToPage(page) {
+    window.location.href = '../menus/' + page + '.html'
+}
