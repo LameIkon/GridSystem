dd6636050086645300a632e47209865c88d5f75f
4sela <adawa21@student.sdu.dk>
2024-11-07 13:36:14 +0100

    Home page

--- js/load-content.js
+++ js/load-content.js
@@ -13,4 +13,5 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     loadHTML('header-container', '../header.html');
+    loadHTML('header-container', 'header.html');
 });
