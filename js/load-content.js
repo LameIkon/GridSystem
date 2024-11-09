9acc3bf8001b6fdf3599f188f3190ab08e0f116e
4sela <adawa21@student.sdu.dk>
2024-11-09 03:53:57 +0100

    Some more effects

--- js/load-content.js
+++ js/load-content.js
@@ -1,3 +1,4 @@
+// AJAX Call
 document.addEventListener('DOMContentLoaded', function () {
     function loadHTML(elementID, filePath) {
         const element = document.getElementById(elementID);
@@ -16,6 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
     loadHTML('header-container', 'header.html');
 });
 
+
+// CSS Class Load
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -27,3 +30,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// CSS Class Load
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('play-now-button');
+    button.addEventListener('animationend', function() {
+        button.classList.add('show-button');
+    });
+});
