c950ec566ec14893ae804b62561bb9ec022d2c50
4sela <adawa21@student.sdu.dk>
2024-11-09 02:45:55 +0100

    home-page-style.css refactor

--- js/load-content.js
+++ js/load-content.js
@@ -15,3 +15,15 @@ document.addEventListener('DOMContentLoaded', function () {
     loadHTML('header-container', '../header.html');
     loadHTML('header-container', 'header.html');
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
