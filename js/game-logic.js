d9ee645e6cf53eb029ee9c2ff653effdad31a661
JohanPedersen11 <Johan.munch.2002@gmail.com>
2024-11-30 18:43:32 +0100

    theory box
    updated json and fetch to get the needed data. Box made right of the game

--- js/game-logic.js
+++ js/game-logic.js
@@ -49,34 +49,6 @@ function initializeGrid(x, y)
     createGrid();
 }
 
-function loadLevel(specifiedId)
-{
-    fetch('../../json/level-layout.json') // Find the location of the json file
-    //fetch('https://johanpedersen11.github.io/jsonData/level-layout.json') // Find the location of the json file
-        .then(response => response.json())
-        .then(info =>
-        {
-            const FILTEREDITEM = info.find(element => element.levelId === specifiedId) // Find the json file with the specific id 'levelId'
-
-            if (FILTEREDITEM) // Take the json and read/use the data
-            {
-                gridX = FILTEREDITEM.x;
-                gridY = FILTEREDITEM.y;
-                maxTurn = FILTEREDITEM.maxTurns;
-                playerPosition = FILTEREDITEM.playerPosition;
-
-                // Setting the Grid variables dynamically in CSS
-                document.documentElement.style.setProperty('--y', gridX);
-                document.documentElement.style.setProperty('--x', gridY);
-
-                GRID_ELEMENT.insertAdjacentHTML('beforeend', FILTEREDITEM.layout); // Print title text as a 'h2'
-                
-                // Run scripts
-                renderPlayer();
-                renderTurnCounter();
-            }
-        });
-}
 
 function createGrid()
 {
@@ -98,7 +70,7 @@ function renderPlayer()
 {
     // Clear previous player positions
     document.querySelectorAll(`.${TILES.PLAYER}`).forEach(player => player.classList.remove(TILES.PLAYER));
-
+    
     let index = playerPosition.y * gridX + playerPosition.x;
     let player_cell = document.getElementById(`${TILES.CELL}-${index}`);
     if (player_cell) {
@@ -142,7 +114,7 @@ function move(direction, steps)
         let newX = playerPosition.x;
         let newY = playerPosition.y;
         let step = 1;
-
+        
         // This for loop makes sure that we only take one step at a time, to prevent 'jumping' over walls
         for (let i = 0; i < steps; i++) {
             switch (direction) {
@@ -150,37 +122,37 @@ function move(direction, steps)
                     // playerPosition.x = Math.max(0, playerPosition.x - steps); // This just looks nice!! maybe it can be used again for floats
                     newX -= step;
                     break;
-                case 'right':
-                    // playerPosition.x = Math.min(gridX - 1, playerPosition.x + steps); // Even better, good concise would do again
-                    newX += step;
-                    break;
-                case 'up':
-                    // playerPosition.y = Math.max(0, playerPosition.y - steps); // OMG can it get any better!
-                    newY -= step;
-                    break;
-                case 'down':
-                    // playerPosition.y = Math.min(gridY - 1, playerPosition.y + steps); // Horrid please remove in the future, who even made this!!
-                    newY += step;
-                    break;
-                default:
-                    console.error('Unknown direction:', direction);
-                    return;
-            }
+                    case 'right':
+                        // playerPosition.x = Math.min(gridX - 1, playerPosition.x + steps); // Even better, good concise would do again
+                        newX += step;
+                        break;
+                        case 'up':
+                            // playerPosition.y = Math.max(0, playerPosition.y - steps); // OMG can it get any better!
+                            newY -= step;
+                            break;
+                            case 'down':
+                                // playerPosition.y = Math.min(gridY - 1, playerPosition.y + steps); // Horrid please remove in the future, who even made this!!
+                                newY += step;
+                                break;
+                                default:
+                                    console.error('Unknown direction:', direction);
+                                    return;
+                                }
+                                
+                                // Collision detection to not go out of bounds
+                                if (newX < 0 || newX >= gridX || newY < 0 || newY >= gridY) {
+                                    return;
+                                }
 
-            // Collision detection to not go out of bounds
-            if (newX < 0 || newX >= gridX || newY < 0 || newY >= gridY) {
-                return;
-            }
-
-            // Collision detection for the obstacles
-            if (!canWalk(newY * gridX + newX)) {
-                return;
+                                // Collision detection for the obstacles
+                                if (!canWalk(newY * gridX + newX)) {
+                                    return;
             }
 
             if (pickedUpKey(newY * gridX + newX)) {
                 keyPickup = true;
             }
-
+            
             if (reachedGoal(newY * gridX + newX)) {
                 console.log('Level Completed');
                 ////////////////// Todo: Goal UI + Functionality
@@ -188,7 +160,7 @@ function move(direction, steps)
         }
         playerPosition.x = newX;
         playerPosition.y = newY;
-
+        
         renderPlayer();
         turn++;
         checkGameOver();
@@ -249,36 +221,36 @@ document.addEventListener('keydown', function (event)
         case 'ArrowLeft':
             move('left', step);
             break;
-        case 'ArrowRight':
-            move('right', step);
-            break;
-        case 'ArrowUp':
-            move('up', step);
-            break;
-        case 'ArrowDown':
-            move('down', step);
-            break;
-    }
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    let distance = 0;
-
-    // Function to retrieve and store the distance from the input field
-    function setDistance() {
-        const distanceInput = document.getElementById("distance-input").value;
-        
-        // Check if the input is not empty and is a valid number
-        if (distanceInput !== "" && !isNaN(distanceInput)) {
-            distance = parseFloat(distanceInput); 
+            case 'ArrowRight':
+                move('right', step);
+                break;
+                case 'ArrowUp':
+                    move('up', step);
+                    break;
+                    case 'ArrowDown':
+                        move('down', step);
+                        break;
+                    }
+                });
+                
+                
+                
+                document.addEventListener('DOMContentLoaded', function() {
+                    let distance = 0;
+                    
+                    // Function to retrieve and store the distance from the input field
+                    function setDistance() {
+                        const distanceInput = document.getElementById("distance-input").value;
+                        
+                        // Check if the input is not empty and is a valid number
+                        if (distanceInput !== "" && !isNaN(distanceInput)) {
+                            distance = parseFloat(distanceInput); 
             console.log("Distance set to:", distance); 
         } else {
             console.log("Invalid number"); 
         }
     }
-
+    
     // Event listener for the button to set distance
     document.getElementById("submit-distance").addEventListener("click", setDistance);
 });
@@ -286,10 +258,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 /*
- *  save() saves all the different tiles into the gameData object.
- *  Right now it is quite hard coded, some refactoring is needed.
- *  It also needs to be into a
- */
+*  save() saves all the different tiles into the gameData object.
+*  Right now it is quite hard coded, some refactoring is needed.
+*  It also needs to be into a
+*/
 
 // #region Save/Load
 function save(mapID)
@@ -298,18 +270,18 @@ function save(mapID)
         console.error("Map name is required to save.");
         return;
     }
-
+    
     // Update the gameData object with the current state
     gameData.player = [PLAYER.item(0).id.split('-')[1], playerPosition];
     saveTiles(TILES.WALL, gameData.walls);
     saveTiles(TILES.DOOR, gameData.doors);
     saveTiles(TILES.KEY, gameData.keys);
     saveTiles(TILES.GOAL, gameData.goal);
-
+    
     // Save game data as JSON under a specific key
     const saveData = JSON.stringify(gameData);
     localStorage.setItem(`${MAP_INDEX}_${mapID}`, saveData);
-
+    
     // Update the map index list
     let mapIndex = JSON.parse(localStorage.getItem(MAP_INDEX)) || [];
     if (!mapIndex.includes(mapID)) {
@@ -320,9 +292,9 @@ function save(mapID)
 }
 
 /*
- *  saveTiles() is responsible for saving the different tiles.
- *  Right now it can only save into arrays.
- */
+*  saveTiles() is responsible for saving the different tiles.
+*  Right now it can only save into arrays.
+*/
 
 function saveTiles(tileName, savePlaceArray)
 {
@@ -342,21 +314,21 @@ function load(mapID)
         console.error("Map name is required to load.");
         return;
     }
-
+    
     const loadData = localStorage.getItem(`${MAP_INDEX}_${mapID}`);
     if (!loadData) {
         console.error(`No saved data found for map: "${mapID}"`);
         return;
     }
-
+    
     // Parse and load the game data
     gameData = JSON.parse(loadData);
     playerPosition = gameData.player[1];
-
+    
     renderGame();
     renderPlayer();
     console.log(`Map "${mapID}" loaded.`);
-
+    
 }
 
 function getSavedMaps()
@@ -368,87 +340,116 @@ function displaySavedMaps()
 {
     const savedMaps = getSavedMaps();
     savedMaps.forEach(mapName =>
-    {
-        console.log(`Saved Map: ${mapName}`);
-    });
-}
-
-function deleteMap(mapID)
-{
-    if (!mapID) {
-        console.error("Map name is required to delete.");
-        return;
+        {
+            console.log(`Saved Map: ${mapName}`);
+        });
     }
-
-    localStorage.removeItem(`${MAP_INDEX}_${mapID}`);
-    let mapIndex = JSON.parse(localStorage.getItem(MAP_INDEX)) || [];
-    mapIndex = mapIndex.filter(name => name !== mapID);
-    localStorage.setItem(MAP_INDEX, JSON.stringify(mapIndex));
-
-    console.log(`Map "${mapName}" deleted.`);
-}
-
-// #endregion
-
-function renderGame()
-{
-    removeTile(TILES.PLAYER);
-    removeTile(TILES.WALL);
-    removeTile(TILES.KEY);
-    removeTile(TILES.DOOR);
-    removeTile(TILES.GOAL);
-
-    addTile(TILES.WALL, gameData.walls);
-    addTile(TILES.KEY, gameData.keys);
-    addTile(TILES.DOOR, gameData.doors);
-    addTile(TILES.GOAL, gameData.goal);
-}
-
-
-function removeTile(tileName)
-{
-    document.querySelectorAll(`.${tileName}`).forEach(tile => tile.classList.remove(tileName)); // Takes all the tiles and removes the name from the class
-}
-
-
-// This needs an array to iterate over
-function addTile(tileName, array)
-{
-    for (let i = 0; i < array.length; i++) {
-        let cell = document.getElementById(`${TILES.CELL}-${array[i]}`);
-        if (cell) {
-            cell.classList.add(tileName);
+    
+    function deleteMap(mapID)
+    {
+        if (!mapID) {
+            console.error("Map name is required to delete.");
+            return;
+        }
+        
+        localStorage.removeItem(`${MAP_INDEX}_${mapID}`);
+        let mapIndex = JSON.parse(localStorage.getItem(MAP_INDEX)) || [];
+        mapIndex = mapIndex.filter(name => name !== mapID);
+        localStorage.setItem(MAP_INDEX, JSON.stringify(mapIndex));
+        
+        console.log(`Map "${mapName}" deleted.`);
+    }
+    
+    // #endregion
+    
+    function renderGame()
+    {
+        removeTile(TILES.PLAYER);
+        removeTile(TILES.WALL);
+        removeTile(TILES.KEY);
+        removeTile(TILES.DOOR);
+        removeTile(TILES.GOAL);
+        
+        addTile(TILES.WALL, gameData.walls);
+        addTile(TILES.KEY, gameData.keys);
+        addTile(TILES.DOOR, gameData.doors);
+        addTile(TILES.GOAL, gameData.goal);
+    }
+    
+    
+    function removeTile(tileName)
+    {
+        document.querySelectorAll(`.${tileName}`).forEach(tile => tile.classList.remove(tileName)); // Takes all the tiles and removes the name from the class
+    }
+    
+    
+    // This needs an array to iterate over
+    function addTile(tileName, array)
+    {
+        for (let i = 0; i < array.length; i++) {
+            let cell = document.getElementById(`${TILES.CELL}-${array[i]}`);
+            if (cell) {
+                cell.classList.add(tileName);
+            }
         }
     }
-}
-
-function resetAllSavedMaps()
-{
-    localStorage.removeItem(MAP_INDEX);
-    playerPosition = {x: 0, y: 0};
-    keyPickup = false;
-    gameData = {player: [], walls: [], keys: [], doors: [], goal: []};
-    renderGame();
-    renderPlayer();
-    turn = 1;
-    renderTurnCounter();
-}
-
-
-function levelSelecting(mapID)
-{
-    document.getElementById('controls').style.display = 'block';
-    load(mapID)
-    deleteGrid()
-    loadLevel(mapID);
-    //initializeGrid(10, 5);
-}
-
-function deleteGrid()
-{
-    GRID_ELEMENT.innerHTML = '';
-}
-
-
-
-
+    
+    function resetAllSavedMaps()
+    {
+        localStorage.removeItem(MAP_INDEX);
+        playerPosition = {x: 0, y: 0};
+        keyPickup = false;
+        gameData = {player: [], walls: [], keys: [], doors: [], goal: []};
+        renderGame();
+        renderPlayer();
+        turn = 1;
+        renderTurnCounter();
+    }
+    
+    
+    function levelSelecting(mapID)
+    {
+        document.getElementById('controls').style.display = 'block';
+        load(mapID)
+        deleteGrid()
+        loadLevel(mapID);
+        //initializeGrid(10, 5);
+    }
+    
+    function deleteGrid()
+    {
+        GRID_ELEMENT.innerHTML = '';
+    }
+    
+    
+    function loadLevel(specifiedId)
+    {
+        fetch('../../json/level-layout.json') // Find the location of the json file
+        //fetch('https://johanpedersen11.github.io/jsonData/level-layout.json') // Find the location of the json file
+            .then(response => response.json())
+            .then(info =>
+            {
+                const FILTEREDITEM = info.find(element => element.levelId === specifiedId) // Find the json file with the specific id 'levelId'
+    
+                if (FILTEREDITEM) // Take the json and read/use the data
+                {
+                    gridX = FILTEREDITEM.gridSize.x;
+                    gridY = FILTEREDITEM.gridSize.y;
+                    maxTurn = FILTEREDITEM.maxTurns;
+                    playerPosition = FILTEREDITEM.playerPosition;
+    
+                    // Setting the Grid variables dynamically in CSS
+                    document.documentElement.style.setProperty('--y', gridX);
+                    document.documentElement.style.setProperty('--x', gridY);
+    
+                    GRID_ELEMENT.insertAdjacentHTML('beforeend', FILTEREDITEM.layout); // Print title text as a 'h2'
+                    
+                    // Run scripts
+                    renderPlayer();
+                    renderTurnCounter();
+                }
+            });
+    }
+    
+    
+    
--- js/level-info.js
+++ js/level-info.js
@@ -1,13 +1,13 @@
 const INFOLIST = document.getElementById('mission-box'); // The location the json file will print its text
-
+const THEORYLIST = document.getElementById('theory-container'); // The location the json file will print its text
 
 function displayLevelInfo(specifiedId)
 {
 // Clear the content before adding new content
 INFOLIST.innerHTML = '';
 
-//fetch('../../json/level-info.json') // Find the location of the json file - Needs a live Server
-fetch('https://johanpedersen11.github.io/jsonData/level-info.json') // Find the location of the json file
+fetch('../../json/level-info.json') // Find the location of the json file - Needs a live Server
+//fetch('https://johanpedersen11.github.io/jsonData/level-info.json') // Find the location of the json file
     .then(response => response.json())
     .then(info =>
         {            
@@ -15,8 +15,18 @@ fetch('https://johanpedersen11.github.io/jsonData/level-info.json') // Find the 
         
         if(FILTEREDITEM) // Take the json and read/use the data
         {
-            INFOLIST.insertAdjacentHTML('beforeend', `<p id="mission">${FILTEREDITEM.title}</p>`); // Print title text as a 'h2'
+            // Mission description
+            INFOLIST.insertAdjacentHTML('beforeend', `<p id="mission">${FILTEREDITEM.title}</p>`); // Print title text as a 'p'
             INFOLIST.insertAdjacentHTML('beforeend', `<p id="mission-description">${FILTEREDITEM.body}</p>`); // Print body text as a 'p'
+            
+             // Theory description
+             THEORYLIST.insertAdjacentHTML('beforeend', `<p id="theory-header">${FILTEREDITEM.theoryTitle}</p>`); // Print title text as a 'p'
+             if (FILTEREDITEM.theoryBox) // if there is an object. loop through and print it
+                {               
+                    Object.entries(FILTEREDITEM.theoryBox).forEach(([key,value]) => {
+                        THEORYLIST.insertAdjacentHTML('beforeend', `<p id="theory-body">${value}</p>`);
+                    });
+                }
         }
         else // Error message
         {
