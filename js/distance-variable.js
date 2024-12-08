960a11466da7da7d13edeba33da7a8374fd2a069
4sela <adawa21@student.sdu.dk>
2024-12-08 23:45:27 +0100

    Put the Variable Box on the left
    Because it should be the primary focus and first thing users see as it's new

--- js/distance-variable.js
+++ js/distance-variable.js
@@ -51,7 +51,7 @@ function buttonMovement(event) {
     let dir = event.srcElement.id.split('-')[1]; // Take the id of the button element splitting it at take the second elemet, which is either 'left', 'right', 'up' or 'down'
     let steps = distance;
     if (!steps) {
-        alert('Please enter a number in the parameter.', 5000);
+        alert('Please set a value to "Steps".', 5000);
         return;
     }
     move(dir, steps);
